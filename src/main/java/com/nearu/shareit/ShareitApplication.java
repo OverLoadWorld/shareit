76aa56b280b27982d3cf4a17775c9361263a3216
howard <zhaozhenhua1@xiaomi.com>
2019-03-05 18:38:55 +0800

    add

--- src/main/java/com/nearu/shareit/ShareitApplication.java
+++ src/main/java/com/nearu/shareit/ShareitApplication.java
@@ -1,20 +1,46 @@
 package com.nearu.shareit;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootApplication
 @RestController
-public class ShareitApplication {
+@Slf4j
+public class ShareitApplication implements CommandLineRunner {
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShareitApplication.class, args);
 	}
 
+
+
 	@RequestMapping("/hi")
 	public String hi() {
 		return "u see see u can do what?";
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+		
+	}
+
+	private void showConnection() throws SQLException {
+		log.info("shit ----> "+dataSource.toString());
+		Connection con = dataSource.getConnection();
+		log.info("shit ----> "+con.toString());
+		con.close();
 	}
 }
